f4b7aa72d5b88308f656b79a2e666ff9e82296e8
ggsno <ogs0518@gmail.com>
2023-12-12 17:31:56 +0900

    Initialize husky

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  env: { es2021: true, node: true },
-  extends: ["eslint:recommended"],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-};
