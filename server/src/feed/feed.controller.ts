5de934b0fb6362f8c27dbf777db5ad5db38afa8c
ggsno <ogs0518@gmail.com>
2023-12-16 03:07:02 +0900

    Refactor: Feed 컨트롤러 통합 - 쿼리 파라미터로 분기

--- server/src/feed/feed.controller.ts
+++ server/src/feed/feed.controller.ts
@@ -5,7 +5,7 @@ import feedService from "./feed.service.js";
 
 const feedController = {
   getFeeds: asyncHandler(async (req, res) => {
-    const { cursor, limit } = req.query;
+    const { cursor, limit, userId, category } = req.query;
     if (cursor === undefined || limit === undefined) {
       throw new CustomError({
         status: 400,
@@ -13,54 +13,30 @@ const feedController = {
       });
     }
 
-    const feeds = await feedService.getFeeds({
-      cursor: Number(cursor),
-      limit: Number(limit),
-    });
-    res.json(feeds);
-  }),
-
-  getUserFeeds: asyncHandler(async (req, res) => {
-    const { userId, cursor, limit } = req.query;
-    if (
-      typeof userId !== "string" ||
-      cursor === undefined ||
-      limit === undefined
-    ) {
-      throw new CustomError({
-        status: 400,
-        message: "userId, cursor 혹은 limit을 전달받지 못했습니다.",
+    if (userId && typeof userId === "string") {
+      const feeds = await feedService.getUserFeeds({
+        userId: new ObjectId(userId),
+        cursor: Number(cursor),
+        limit: Number(limit),
       });
-    }
 
-    const feeds = await feedService.getUserFeeds({
-      userId: new ObjectId(userId),
-      cursor: Number(cursor),
-      limit: Number(limit),
-    });
-
-    res.json(feeds);
-  }),
-
-  getCategoryFeeds: asyncHandler(async (req, res) => {
-    const { category, cursor, limit } = req.query;
-    if (
-      typeof category !== "string" ||
-      cursor === undefined ||
-      limit === undefined
-    ) {
-      throw new CustomError({
-        status: 400,
-        message: "category, cursor 혹은 limit을 전달받지 못했습니다.",
+      res.json(feeds);
+    } else if (category && typeof category === "string") {
+      const feeds = await feedService.getCategoryFeeds({
+        category: new ObjectId(category),
+        cursor: Number(cursor),
+        limit: Number(limit),
       });
-    }
 
-    const feeds = await feedService.getCategoryFeeds({
-      category: new ObjectId(category),
-      cursor: Number(cursor),
-      limit: Number(limit),
-    });
-    res.json(feeds);
+      res.json(feeds);
+    } else {
+      const feeds = await feedService.getFeeds({
+        cursor: Number(cursor),
+        limit: Number(limit),
+      });
+
+      res.json(feeds);
+    }
   }),
 };
 
