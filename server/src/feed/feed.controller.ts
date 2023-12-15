b7a2f606b264168e806d89a4f80cdc514a372c0a
ggsno <ogs0518@gmail.com>
2023-12-16 02:27:46 +0900

    Feat: #8 유저 피드, 카테고리 피드 api 구현

--- server/src/feed/feed.controller.ts
+++ server/src/feed/feed.controller.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import asyncHandler from "../middleware/asyncHandler.js";
 import { CustomError } from "../middleware/errorHandler.js";
 import feedService from "./feed.service.js";
@@ -13,6 +14,49 @@ const feedController = {
     }
 
     const feeds = await feedService.getFeeds({
+      cursor: Number(cursor),
+      limit: Number(limit),
+    });
+    res.json(feeds);
+  }),
+
+  getUserFeeds: asyncHandler(async (req, res) => {
+    const { userId, cursor, limit } = req.query;
+    if (
+      typeof userId !== "string" ||
+      cursor === undefined ||
+      limit === undefined
+    ) {
+      throw new CustomError({
+        status: 400,
+        message: "userId, cursor 혹은 limit을 전달받지 못했습니다.",
+      });
+    }
+
+    const feeds = await feedService.getUserFeeds({
+      userId: new ObjectId(userId),
+      cursor: Number(cursor),
+      limit: Number(limit),
+    });
+
+    res.json(feeds);
+  }),
+
+  getCategoryFeeds: asyncHandler(async (req, res) => {
+    const { category, cursor, limit } = req.query;
+    if (
+      typeof category !== "string" ||
+      cursor === undefined ||
+      limit === undefined
+    ) {
+      throw new CustomError({
+        status: 400,
+        message: "category, cursor 혹은 limit을 전달받지 못했습니다.",
+      });
+    }
+
+    const feeds = await feedService.getCategoryFeeds({
+      category: new ObjectId(category),
       cursor: Number(cursor),
       limit: Number(limit),
     });
--- server/src/feed/feed.service.ts
+++ server/src/feed/feed.service.ts
@@ -1,8 +1,31 @@
+import { Types } from "mongoose";
 import FeedModel from "./feed.model.js";
 
 const feedService = {
   async getFeeds({ cursor, limit }: { cursor: number; limit: number }) {
     const feeds = await FeedModel.find({}).skip(cursor).limit(limit);
+
+    return feeds;
+  },
+
+  async getUserFeeds(props: {
+    userId: Types.ObjectId;
+    cursor: number;
+    limit: number;
+  }) {
+    const { userId, cursor, limit } = props;
+    const feeds = await FeedModel.find({ userId }).skip(cursor).limit(limit);
+
+    return feeds;
+  },
+
+  async getCategoryFeeds(props: {
+    category: Types.ObjectId;
+    cursor: number;
+    limit: number;
+  }) {
+    const { category, cursor, limit } = props;
+    const feeds = await FeedModel.find({ category }).skip(cursor).limit(limit);
 
     return feeds;
   },
