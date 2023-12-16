4fc0b31a77c93775d8e245b5cdb60fc06a1eeecf
ggsno <ogs0518@gmail.com>
2023-12-16 15:05:18 +0900

    Refactor: 쿼리 파라미터 검증 분리

--- server/src/feed/feed.controller.ts
+++ server/src/feed/feed.controller.ts
@@ -1,29 +1,22 @@
 import { ObjectId } from "mongodb";
 import asyncHandler from "../middleware/asyncHandler.js";
-import { CustomError } from "../middleware/errorHandler.js";
 import feedService from "./feed.service.js";
 
 const feedController = {
   getFeeds: asyncHandler(async (req, res) => {
     const { cursor, limit, userId, category } = req.query;
-    if (cursor === undefined || limit === undefined) {
-      throw new CustomError({
-        status: 400,
-        message: "cursor 혹은 limit을 전달받지 못했습니다.",
-      });
-    }
 
-    if (userId && typeof userId === "string") {
+    if (userId) {
       const feeds = await feedService.getUserFeeds({
-        userId: new ObjectId(userId),
+        userId: new ObjectId(userId as string),
         cursor: Number(cursor),
         limit: Number(limit),
       });
 
       res.json(feeds);
-    } else if (category && typeof category === "string") {
+    } else if (category) {
       const feeds = await feedService.getCategoryFeeds({
-        category: new ObjectId(category),
+        category: new ObjectId(category as string),
         cursor: Number(cursor),
         limit: Number(limit),
       });
