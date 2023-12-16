d92c4ba1a3028d4e8fbd4bb304a25353d1c9b513
HSHyeon <hshyeon86@gmail.com>
2023-12-16 17:28:14 +0900

    refactor #3 : 코멘트 추가

--- client/src/components/Profile/Follow.tsx
+++ client/src/components/Profile/Follow.tsx
@@ -1,7 +1,7 @@
 import { useFollow } from "./Follow.hooks";
 import * as S from "./Profile.styles";
 
-const userid = "657ae3e09566ca0e802a2676";
+const userid = "657ae3e09566ca0e802a2676"; //쿼리스트링으로 받아와야 하는 값
 
 export default function Follow() {
   const { follows, isLoading, isError, error } = useFollow(userid);
--- client/src/components/Profile/ProfileTop.tsx
+++ client/src/components/Profile/ProfileTop.tsx
@@ -2,10 +2,11 @@ import { useFollow } from "./Follow.hooks";
 import { useUser } from "./User.hooks";
 import * as S from "./Profile.styles";
 
-let temp = 0;
+let temp = 0; //임시 변수
+//TODO: 내 프로필인지 다른 사람 프로필인지, 팔언팔 상태에 대한 분기 처리 필요
+
 const userid = "657ae3e09566ca0e802a2676"; //쿼리스트링으로 받아와야 하는 값
-//내 프로필인지 다른 사람 프로필인지에 대한 분기 처리 필요
-//테스트 ui 컴포넌트 분리 필요
+//TODO: 테스트 ui 컴포넌트 분리 필요
 
 export default function ProfileTop() {
   const { follows, deleteFollow, postFollow, isLoading, isError, error } =
