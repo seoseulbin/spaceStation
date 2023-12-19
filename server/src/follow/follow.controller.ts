8a44d4c8f1efa0e0933a90168c01cf0513f0105f
HSHyeon <hshyeon86@gmail.com>
2023-12-19 18:51:53 +0900

    feat #14: 야매 soft delete

--- server/src/follow/follow.controller.ts
+++ server/src/follow/follow.controller.ts
@@ -49,18 +49,24 @@ const followController = {
       const { follower } = req.body;
       const userToken = req.cookies.service_token;
       const following = decodeTokenPayload(userToken)["user_id"];
-
+      const result = await followService.checkFollowing(follower, following);
       if (!follower)
         throw new CustomError({
           status: 400,
           message: "전달된 내용이 없습니다.",
         });
-      else if (follower === following)
+      if (follower === following)
         throw new CustomError({
           status: 400,
           message: "자신을 팔로우 할 수 없습니다.",
         });
-      followService.postFollow({ follower, following });
+      if (!result) {
+        followService.postFollow({ follower, following });
+      }
+      if (result && result.deletedAt !== null) {
+        await followService.revertDeletedFollower(follower, following);
+      }
+
       res.status(200).end();
     },
   ),
@@ -75,13 +81,23 @@ const followController = {
         status: 400,
         message: "유효하지 않은 ID입니다.",
       });
-    else if (follower === following)
+    if (follower === following)
       throw new CustomError({
         status: 400,
         message: "자신을 언팔로우 할 수 없습니다.",
       });
-    await followService.deleteFollow(follower, following);
-    res.status(200).end();
+    const result = await followService.deleteFollow(follower, following);
+    if (!result) {
+      throw new CustomError({
+        status: 400,
+        message: "팔로우 취소에 실패했습니다.",
+      });
+    }
+    res.status(200).json({
+      message: "회원 탈퇴에 성공했습니다.",
+      follow: result,
+    });
   }),
 };
+
 export default followController;
