192a69bf2b2827fb169b6e8667e38c7bfd20e8a8
HSHyeon <hshyeon86@gmail.com>
2023-12-20 18:41:36 +0900

    Refactor #14:[FE] errorboundary, axiostinstance

--- client/src/components/Profile/ProfileTop.tsx
+++ client/src/components/Profile/ProfileTop.tsx
@@ -7,33 +7,28 @@ import { useFollow } from "../Follow/Follow.hooks";
 import { useUser } from "../User/User.hooks";
 import FollowButton from "../Follow/FollowButton";
 import { Link } from "react-router-dom";
+import ApiBoundary from "../common/ApiBoundary";
 
 export default function ProfileTop({ userId }: { userId: string }) {
+  return (
+    <ApiBoundary>
+      <ApiComponent userId={userId} />
+    </ApiBoundary>
+  );
+}
+
+function ApiComponent({ userId }: { userId: string }) {
   const [isFollowModalOpen, setIsFollowModalOpen] = useState(false);
   const [followState, setIsFollowState] = useState(false);
   const localUserData = localStorage.getItem("currentUser");
 
-  const { follows, isLoading, isError, error } = useFollow(userId);
+  const { follows } = useFollow(userId);
   const { user } = useUser(userId);
-
-  if (isLoading) return "loading...";
-  if (isError) return error.message;
 
   const handleFollowModalOpen = (state: boolean) => {
     setIsFollowModalOpen(true);
     setIsFollowState(state);
   };
-  const HandlerFollowButton = () => {
-    if (localUserData && JSON.parse(localUserData).userId === userId) {
-      return (
-        <Link to="/profile/update">
-          <input type="button" value={"프로필 수정"} />
-        </Link>
-      );
-    }
-    return null;
-  };
-  const ProfileButton = () => <div>{HandlerFollowButton()}</div>;
 
   return (
     <>
@@ -56,8 +51,13 @@ export default function ProfileTop({ userId }: { userId: string }) {
                 <span>{follows?.following?.length}</span> 팔로잉
               </S.Following>
             </S.Follow>
-            <FollowButton currentUserId={userId} />
-            <ProfileButton />
+            {localUserData && JSON.parse(localUserData).userId === userId ? (
+              <Link to="/profile/update">
+                <input type="button" value={"프로필 수정"} />
+              </Link>
+            ) : (
+              <FollowButton userId={userId} />
+            )}
           </div>
         </div>
       </S.Container>
--- client/src/components/User/User.tsx
+++ client/src/components/User/User.tsx
@@ -1,12 +1,21 @@
 import * as S from "./User.styles";
 import { useUser } from "./User.hooks";
 import { Link } from "react-router-dom";
+import ApiBoundary from "../common/ApiBoundary";
 
-export default function User({ currentUserId }: { currentUserId: string }) {
-  const { user } = useUser(currentUserId);
+export default function User({ userId }: { userId: string }) {
+  return (
+    <ApiBoundary>
+      <ApiComponent userId={userId} />
+    </ApiBoundary>
+  );
+}
+
+function ApiComponent({ userId }: { userId: string }) {
+  const { user } = useUser(userId);
 
   return (
-    <Link to={`/profile?id=${currentUserId}`}>
+    <Link to={`/profile?id=${userId}`}>
       <S.UserContainer>
         <S.ProfileImg src={user?.profileImgUrl} />
         <span>{user?.nickname} </span>
