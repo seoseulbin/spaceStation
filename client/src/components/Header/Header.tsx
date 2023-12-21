3831db90dd9c37bf0e4b119638af292df35ec47b
[SW7] 정현지 <blackit14@gmail.com>
2023-12-21 12:29:38 +0900

    feat: #1 Header UI 작성 & props 속성 설정

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,3 +1,52 @@
-export default function Header() {
-  return <div></div>;
+import { SyntheticEvent } from "react";
+import * as S from "./Header.styles";
+import { IoIosArrowBack } from "react-icons/io";
+
+interface HeaderType {
+  backArrow: boolean;
+  headerTitle?: string;
+  headerUrl?: string | undefined;
+  isFunctionAcitve?: boolean;
+  functionTitle?: string | undefined;
+  functionTitleColor?: string | undefined;
+  onClickFunction?: (e: SyntheticEvent<HTMLDivElement>) => void | undefined;
+}
+
+export default function Header({
+  backArrow,
+  headerTitle = "",
+  headerUrl,
+  isFunctionAcitve,
+  functionTitle,
+  functionTitleColor,
+  onClickFunction,
+}: HeaderType) {
+  //뒤로가기 함수
+  const handleBackArrowFunction = () => {
+    window.history.back();
+  };
+
+  return (
+    <S.Container>
+      <S.ContainerLeftDiv>
+        {backArrow ? (
+          <S.ArrowButtonDiv onClick={handleBackArrowFunction}>
+            <IoIosArrowBack size="35" />
+          </S.ArrowButtonDiv>
+        ) : (
+          <></>
+        )}
+        {headerTitle.length > 0 ? (
+          <S.HeaderTitleDiv>{headerTitle}</S.HeaderTitleDiv>
+        ) : (
+          <S.HeaderTitleDiv>
+            <img src={headerUrl} />
+          </S.HeaderTitleDiv>
+        )}
+      </S.ContainerLeftDiv>
+      <S.ContainerRightDiv onClick={onClickFunction} color={functionTitleColor}>
+        {isFunctionAcitve && functionTitle}
+      </S.ContainerRightDiv>
+    </S.Container>
+  );
 }
