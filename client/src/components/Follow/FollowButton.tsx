74be18520eb0536c6231361ed6ca885930672364
HSHyeon <hshyeon86@gmail.com>
2023-12-22 20:57:38 +0900

    Refactor #24: dev 충돌 해결

--- client/src/components/Follow/FollowButton.tsx
+++ client/src/components/Follow/FollowButton.tsx
@@ -2,6 +2,8 @@ import { storage } from "@/global/storage";
 import { useFollow } from "./Follow.hooks";
 import toast from "react-hot-toast";
 import ApiBoundary from "../common/ApiBoundary";
+import { useNavigate } from "react-router-dom";
+import { PATH } from "@/global/constants";
 
 export default function FollowButton({ userId }: { userId: string }) {
   return (
@@ -12,15 +14,17 @@ export default function FollowButton({ userId }: { userId: string }) {
 }
 
 function ApiComponent({ userId }: { userId: string }) {
+  const navigate = useNavigate();
   const { checkFollow, postFollow, deleteFollow } = useFollow(userId);
-  const localUserData = storage.get("currentUser");
-  const isLoggedIn = !!localUserData;
-  const isCurrentUser = localUserData && localUserData.userId === userId;
+  const currentUser = storage.get("currentUser");
+  const isLoggedIn = !!currentUser;
+  const isCurrentUser = currentUser && currentUser.userId === userId;
 
   const handleFollowClick = async () => {
     try {
       if (!isLoggedIn) {
         toast.error("팔로우하려면 로그인해주세요");
+        navigate(PATH.login);
         return;
       }
 
