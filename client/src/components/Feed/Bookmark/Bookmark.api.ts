39a3f84bb5e3637e66df82d37ad0bc47b765c52b
[SW7] 정현지 <blackit14@gmail.com>
2023-12-23 23:53:54 +0900

    #28 fix: [FE] API 버그 수정

--- client/src/components/Feed/Bookmark/Bookmark.api.ts
+++ client/src/components/Feed/Bookmark/Bookmark.api.ts
@@ -1,10 +1,24 @@
 import { BookmarkType } from "./Bookmark.type";
 import { axiosInstance } from "../../../global/axiosInstance";
+import { FeedType } from "../Feed.type";
 
 export const bookmarkAPI = {
   async getBookmarkByFeedId(feedId: string) {
     const res = await axiosInstance.get<BookmarkType[]>(`/bookmarks/${feedId}`);
     return res.data;
+  },
+
+  //TODO : 마이페이지 북마크 리스크만 보이기
+  async getBookmarkFeedByUserId(props: {
+    userId: string;
+    cursor: number;
+    limit: number;
+  }) {
+    const { userId, cursor, limit } = props;
+    const { data } = await axiosInstance.get<FeedType[]>(
+      `/?userId=${userId}&cursor=${cursor}&limit=${limit}`,
+    );
+    return { data, nextCursor: cursor + limit };
   },
 
   async postBookmark(feedId: string) {
