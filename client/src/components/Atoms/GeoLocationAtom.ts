7f1be90c812b8d3674d8877dc11e3978941a00fa
[SW7] 김지훈 <lanslot84@gmail.com>
2023-12-27 22:31:03 +0900

    Refactor #33 : geoLocation atom 초기값 변경

--- client/src/components/Atoms/GeoLocationAtom.ts
+++ client/src/components/Atoms/GeoLocationAtom.ts
@@ -1,13 +1,33 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 import { GeoLocationType } from "@/global/types/geoLocation";
 
 export const geoLocationAtom = atom<GeoLocationType>({
   key: "geoLocation",
   default: {
-    content: undefined,
+    content: "",
     position: {
-      lat: undefined,
-      lng: undefined,
+      lat: 0,
+      lng: 0,
+    },
+  },
+});
+
+export const geoLocationContent = selector({
+  key: "geoLocationAtomContent",
+  get: ({ get }) => {
+    const geoLocation = get(geoLocationAtom);
+
+    return geoLocation.content;
+  },
+});
+
+export const geoLocationMarkerAtom = atom<GeoLocationType>({
+  key: "geoLocationMarker",
+  default: {
+    content: "",
+    position: {
+      lat: 0,
+      lng: 0,
     },
   },
 });
