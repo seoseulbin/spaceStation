8e0e8ff1ac9cb931363c14445ff55b0db1b717ac
HSHyeon <hshyeon86@gmail.com>
2023-12-27 23:17:30 +0900

    Style #42: [FE] not found

--- client/src/pages/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage.tsx
@@ -1,4 +1,31 @@
-// TODO: 스타일링
+import React from "react";
+import styled from "styled-components";
+
+const StylesdLayout = styled.div`
+  max-width: ${({ theme }) => theme.size.maxWidth}px;
+  margin: 0 auto;
+  background-color: ${({ theme }) => theme.colors.background};
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  display: flex;
+  right: 0;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NotFoundImg = styled.img`
+  width: 25vw;
+  position: fixed;
+  max-width: 180px;
+`;
+
 export default function NotFoundPage() {
-  return <>페이지를 찾지 못했습니다.</>;
+  return (
+    <>
+      <StylesdLayout>
+        <NotFoundImg src="../notfound.png" />
+      </StylesdLayout>
+    </>
+  );
 }
