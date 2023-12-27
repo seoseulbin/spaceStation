7d614bb6d353c99f9181d98abcdbeb1ae88753ad
ggsno <ogs0518@gmail.com>
2023-12-27 20:48:17 +0900

    Feat: #41 유저 검색 api 구현

--- server/src/user/user.router.ts
+++ server/src/user/user.router.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import userController from "./user.controller.js";
+import { validateQueryParams } from "../middleware/validation/queryParams.js";
 
 const userRouter = Router();
 
-userRouter.get("/:userid", userController.getUser);
 userRouter.get("/me", userController.getUser);
+userRouter.get(
+  "/search/:query",
+  validateQueryParams([
+    { key: "limit", isNumber: true },
+    { key: "cursor", isNumber: true },
+  ]),
+  userController.getUsersByQuery,
+);
+userRouter.get("/:userid", userController.getUser);
 userRouter.put("/", userController.updateUser);
 
 export default userRouter;
