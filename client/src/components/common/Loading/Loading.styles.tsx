bc896d842bec8927cb8435db16d7d5b29a3efa6f
seoseulbin <80270808+seoseulbin@users.noreply.github.com>
2023-12-27 12:24:07 +0900

    FE: 로딩창 구현

--- client/src/components/common/Loading/Loading.styles.tsx
+++ client/src/components/common/Loading/Loading.styles.tsx
@@ -1,52 +1,33 @@
 import styled, { keyframes } from "styled-components";
 
-export const Background = styled.div`
+export const LoadingText = styled.p`
+  color: black;
   position: fixed;
-  top: 0;
-  left: 0;
+
+  top: 50%;
+  left: 46%;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
+
+  z-index: -1;
 `;
 
-export const LoadingText = styled.p`
-  color: white;
-  position: fixed;
-`;
-
-const rotate = keyframes`
+const spin = keyframes`
   to {
     transform: rotate(360deg);
   }
 `;
 
-export const RotateLolletWrapper = styled.div`
-  position: fixed;
-  width: 350px;
-  height: 350px;
-  top: 150px;
-  border-radius: 50%;
-  background-color: transparent;
-  animation: ${rotate} 2s linear infinite;
-  overflow: hidden;
-`;
+export const Spinner = styled.div`
+  margin: calc(50% - 25px) auto;
+  width: 50px;
+  height: 50px;
+  border: 7px solid rgba(0, 0, 0, 1);
+  box-sizing: border-box;
 
-export const RotateLolletSection = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  clip-path: polygon(50% 50%, 100% 0%, 100% 100%);
-  background-color: ${(props) => props.color};
-`;
+  border-top-color: white;
+  border-bottom-color: white;
 
-export const Rect = styled.div`
-  position: fixed;
-  width: 400px;
-  height: 750px;
-  top: 320px;
-  background-color: brown;
+  border-radius: 100%;
+  animation: ${spin} 1s ease-in-out infinite; // Use the keyframes variable here
 `;
--- client/src/components/common/Loading/Loading.tsx
+++ client/src/components/common/Loading/Loading.tsx
@@ -4,10 +4,8 @@ import * as S from "./Loading.styles";
 export default function Loading() {
   return (
     <>
-      <S.Background>
-        <S.Rect />
-        <S.LoadingText>잠시만 기다려 주세요.</S.LoadingText>
-      </S.Background>
+      <S.Spinner />
+      <S.LoadingText>잠시만 기다려 주세요.</S.LoadingText>
     </>
   );
 }
