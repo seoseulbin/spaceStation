494d43136e8b3d696cdc43728a6a107012dd154b
[SW7] 김지훈 <lanslot84@gmail.com>
2023-12-28 18:17:14 +0900

    Refactor : [FE] feed 삭제 시 withcredential 옵션 추가

--- client/src/components/Feed/FeedOption/FeedOption.api.ts
+++ client/src/components/Feed/FeedOption/FeedOption.api.ts
@@ -3,7 +3,9 @@ import { axiosInstance } from "@/global/axiosInstance";
 const feedAPI = {
   deleteFeed: async (_id: string) => {
     try {
-      const response = await axiosInstance.delete(`/feeds/${_id}`);
+      const response = await axiosInstance.delete(`/feeds/${_id}`, {
+        withCredentials: true,
+      });
       return response.data;
     } catch (error) {
       if (error instanceof Error) console.log(error.message);
