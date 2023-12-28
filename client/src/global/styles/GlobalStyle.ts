5bd024f456dcd1bc363ee9e750c45ab542935f31
seoseulbin <80270808+seoseulbin@users.noreply.github.com>
2023-12-28 21:40:57 +0900

    [FE] 스크롤바 커스텀

--- client/src/global/styles/GlobalStyle.ts
+++ client/src/global/styles/GlobalStyle.ts
@@ -12,6 +12,17 @@ const GlobalStyle = createGlobalStyle`
   body {
     box-sizing:border-box;
     font-family: NotoSansKR, Arial, Helvetica, sans-serif;
+
+    &::-webkit-scrollbar {
+    width: 6px;
+    height: 5px;
+    border-radius: 6px;
+    background: rgba(0, 0, 0, 0);
+  }
+  &::-webkit-scrollbar-thumb {
+    background: ${({ theme }) => theme.colors.sub};
+    border-radius: 6px;
+  }
   }
   
   a{
