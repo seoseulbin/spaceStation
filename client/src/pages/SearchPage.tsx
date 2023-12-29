cad8b761eb9be379bcc9026284fabd21b317a5fc
[SW7] 김지훈 <lanslot84@gmail.com>
2023-12-30 03:08:57 +0900

    Feat : [FE] 검색 화면 진입 시 검색 필드에 autoFocus 옵션 추가

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -44,6 +44,7 @@ export default function SearchPage() {
           <IoIosArrowBack size="35" />
         </ArrowButtonDiv>
         <SearchInputBar
+          autoFocus
           onChange={onTypeQueryInput}
           value={queryInput}
           placeholder="검색어를 입력해주세요 ദ്ദി˙∇˙)ว"
